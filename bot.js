4294506fa4e320390ae6c7ce19b22a8aa1529cc2
inventiontime <66506821+inventiontime@users.noreply.github.com>
2020-10-17 11:08:06 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -156,6 +156,7 @@ client.on('message', message => {
         case "exit":
         case "e":
             if((message.author == player1 || message.author == player2) && gameState != GameState[0] && gameState != GameState[1])
+                reset();
             break;
     }
 });
